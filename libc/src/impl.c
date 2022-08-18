9ce4acb3149da035218f3f377096af53fa954b51
YI <uuuuuu@protonmail.com>
2022-08-18 14:47:29 +0800

    add floor

--- libc/src/impl.c
+++ libc/src/impl.c
@@ -1706,6 +1706,31 @@ double ldexp(double x, int n)
 	return scalbn(x, n);
 }
 
+double floor(double x)
+{
+  #define EPS DBL_EPSILON
+  static const double toint = 1/EPS;
+	union {double f; uint64_t i;} u = {x};
+	int e = u.i >> 52 & 0x7ff;
+	double y;
+
+	if (e >= 0x3ff+52 || x == 0)
+		return x;
+	/* y = int(x) - x, where int(x) is an integer neighbor of x */
+	if (u.i >> 63)
+		y = x - toint + toint - x;
+	else
+		y = x + toint - toint - x;
+	/* special case because of non-nearest rounding modes */
+	if (e <= 0x3ff-1) {
+		FORCE_EVAL(y);
+		return u.i >> 63 ? -1 : 0;
+	}
+	if (y > 0)
+		return x + y - 1;
+	return x + y;
+}
+
 int strcoll (const char *l, const char *r) {
   return strcmp(l, r);
 }
