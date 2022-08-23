e132dffbfb616f637b496bbf67546f4df851ea3e
YI <uuuuuu@protonmail.com>
2022-08-23 16:03:49 +0800

    add strtod, strtof, strtold

--- libc/src/impl.c
+++ libc/src/impl.c
@@ -23,7 +23,6 @@
 #include <locale.h>
 #include <setjmp.h>
 
-#include "mock_impl.h"
 /*
  * The implementation here is based on musl-libc with modifications for our
  * use case. The original musl-libc follows MIT license, thanks to the authors
@@ -1843,5 +1842,202 @@ const int32_t **__ctype_tolower_loc(void)
 char *getenv(const char *name)
 {
 	return 0;
+}
+
+int isspace(int c)
+{
+    return c == ' ' || (unsigned)c-'\t' < 5;
+}
+
+
+// Copied from dietlibc
+float strtof(const char* s, char** endptr) {
+    register const char*  p     = s;
+    register float        value = 0.;
+    int                   sign  = +1;
+    float                 factor;
+    unsigned int          expo;
+
+    while ( isspace(*p) )
+        p++;
+
+    switch (*p) {
+    case '-': sign = -1;	/* fall through */
+    case '+': p++;
+    default : break;
+    }
+
+    while ( (unsigned int)(*p - '0') < 10u )
+        value = value*10 + (*p++ - '0');
+
+    if ( *p == '.' ) {
+        factor = 1.;
+
+        p++;
+        while ( (unsigned int)(*p - '0') < 10u ) {
+            factor *= 0.1;
+            value  += (*p++ - '0') * factor;
+        }
+    }
+
+    if ( (*p | 32) == 'e' ) {
+        expo   = 0;
+        factor = 10.L;
+
+        switch (*++p) {                 // ja hier weiß ich nicht, was mindestens nach einem 'E' folgenden MUSS.
+        case '-': factor = 0.1;	/* fall through */
+        case '+': p++;
+                  break;
+        case '0': case '1': case '2': case '3': case '4': case '5': case '6': case '7': case '8': case '9':
+                  break;
+        default : value = 0.L;
+                  p     = s;
+                  goto done;
+        }
+
+        while ( (unsigned int)(*p - '0') < 10u )
+            expo = 10 * expo + (*p++ - '0');
+
+        while ( 1 ) {
+            if ( expo & 1 )
+                value *= factor;
+            if ( (expo >>= 1) == 0 )
+                break;
+            factor *= factor;
+        }
+    }
+
+done:
+    if ( endptr != NULL )
+        *endptr = (char*)p;
+
+    return value * sign;
+}
+
+#define ldbltype long double
+double strtod(const char* s, char** endptr) {
+    register const char*  p     = s;
+    register ldbltype     value = 0.;
+    int                   sign  = +1;
+    ldbltype              factor;
+    unsigned int          expo;
+
+    while ( isspace(*p) )
+        p++;
+
+    switch (*p) {
+    case '-': sign = -1;	/* fall through */
+    case '+': p++;
+    default : break;
+    }
+
+    while ( (unsigned int)(*p - '0') < 10u )
+        value = value*10 + (*p++ - '0');
+
+    if ( *p == '.' ) {
+        factor = 1.;
+
+        p++;
+        while ( (unsigned int)(*p - '0') < 10u ) {
+            factor *= 0.1;
+            value  += (*p++ - '0') * factor;
+        }
+    }
+
+    if ( (*p | 32) == 'e' ) {
+        expo   = 0;
+        factor = 10.;
+
+        switch (*++p) {                 // ja hier weiß ich nicht, was mindestens nach einem 'E' folgenden MUSS.
+        case '-': factor = 0.1;	/* fall through */
+        case '+': p++;
+                  break;
+        case '0': case '1': case '2': case '3': case '4': case '5': case '6': case '7': case '8': case '9':
+                  break;
+        default : value = 0.;
+                  p     = s;
+                  goto done;
+        }
+
+        while ( (unsigned int)(*p - '0') < 10u )
+            expo = 10 * expo + (*p++ - '0');
+
+        while ( 1 ) {
+            if ( expo & 1 )
+                value *= factor;
+            if ( (expo >>= 1) == 0 )
+                break;
+            factor *= factor;
+        }
+    }
+
+done:
+    if ( endptr != NULL )
+        *endptr = (char*)p;
+
+    return value * sign;
+}
+
+long double strtold(const char* s, char** endptr) {
+    register const char*  p     = s;
+    register long double  value = 0.L;
+    int                   sign  = +1;
+    long double           factor;
+    unsigned int          expo;
+
+    while ( isspace(*p) )
+        p++;
+
+    switch (*p) {
+    case '-': sign = -1;	/* fall through */
+    case '+': p++;
+    default : break;
+    }
+
+    while ( (unsigned int)(*p - '0') < 10u )
+        value = value*10 + (*p++ - '0');
+
+    if ( *p == '.' ) {
+        factor = 1.;
+
+        p++;
+        while ( (unsigned int)(*p - '0') < 10u ) {
+            factor *= 0.1;
+            value  += (*p++ - '0') * factor;
+        }
+    }
+
+    if ( (*p | 32) == 'e' ) {
+        expo   = 0;
+        factor = 10.L;
+
+        switch (*++p) {                 // ja hier weiß ich nicht, was mindestens nach einem 'E' folgenden MUSS.
+        case '-': factor = 0.1;	/* fall through */
+        case '+': p++;
+                  break;
+        case '0': case '1': case '2': case '3': case '4': case '5': case '6': case '7': case '8': case '9':
+                  break;
+        default : value = 0.L;
+                  p     = s;
+                  goto done;
+        }
+
+        while ( (unsigned int)(*p - '0') < 10u )
+            expo = 10 * expo + (*p++ - '0');
+
+        while ( 1 ) {
+            if ( expo & 1 )
+                value *= factor;
+            if ( (expo >>= 1) == 0 )
+                break;
+            factor *= factor;
+        }
+    }
+
+done:
+    if ( endptr != NULL )
+        *endptr = (char*)p;
+
+    return value * sign;
 }
 #endif  // __CKB_IMPL_INCLUDED__
