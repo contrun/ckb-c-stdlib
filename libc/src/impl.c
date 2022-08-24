da188889021a36feb77cbb07eefbc1a5572725da
YI <uuuuuu@protonmail.com>
2022-08-24 10:31:38 +0800

    add islower, isupper, tolower, toupper

--- libc/src/impl.c
+++ libc/src/impl.c
@@ -1746,6 +1746,28 @@ char *strerror(int e)
   return errorstr;
 }
 
+int islower(int c)
+{
+	return (unsigned)c-'a' < 26;
+}
+
+int isupper(int c)
+{
+	return (unsigned)c-'A' < 26;
+}
+
+int tolower(int c)
+{
+	if (isupper(c)) return c | 32;
+	return c;
+}
+
+int toupper(int c)
+{
+	if (islower(c)) return c & 0x5f;
+	return c;
+}
+
 #define X(x) (((x)/256 | (x)*256) % 65536)
 
 const unsigned short **__ctype_b_loc(void)
