e9772965fccae42fc8bc3f7bb6e2e4eb758f7012
YI <uuuuuu@protonmail.com>
2023-10-23 20:46:41 +0800

    add example to reproduce float conversion bug in llvm compiler runtime

---
+++ examples/float_conversion_bug.c
@@ -0,0 +1,49 @@
+#define CKB_C_STDLIB_PRINTF 1
+#include <stdio.h>
+
+#define SYS_ckb_debug 2177
+
+#define memory_barrier() asm volatile("fence" ::: "memory")
+
+static inline long __internal_syscall(long n, long _a0, long _a1, long _a2,
+                                      long _a3, long _a4, long _a5) {
+  register long a0 asm("a0") = _a0;
+  register long a1 asm("a1") = _a1;
+  register long a2 asm("a2") = _a2;
+  register long a3 asm("a3") = _a3;
+  register long a4 asm("a4") = _a4;
+  register long a5 asm("a5") = _a5;
+  register long syscall_id asm("a7") = n;
+
+  asm volatile("scall"
+               : "+r"(a0)
+               : "r"(a1), "r"(a2), "r"(a3), "r"(a4), "r"(a5), "r"(syscall_id));
+  /*
+   * Syscalls might modify memory sent as pointer, adding a barrier here ensures
+   * gcc won't do incorrect optimization.
+   */
+  memory_barrier();
+
+  return a0;
+}
+
+#define syscall(n, a, b, c, d, e, f)                                           \
+  __internal_syscall(n, (long)(a), (long)(b), (long)(c), (long)(d), (long)(e), \
+                     (long)(f))
+
+int ckb_debug(const char *s) {
+  return syscall(SYS_ckb_debug, s, 0, 0, 0, 0, 0);
+}
+
+__attribute__((noinline)) double convert_integer_to_number(long long i) {
+  printf("Integer before conversion: %lld\n", i);
+  double n = (double)(i);
+  printf("Number after conversion: %.6f\n", n);
+  return n;
+}
+
+int main() {
+  long long i = -9223372036854775808uLL;
+  convert_integer_to_number(i);
+  return 0;
+}
